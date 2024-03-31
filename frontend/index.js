04f6fec64c68fc1d25ae81d59fce1842f252567a
Abimbola <aiyedogbonabimbola3@gmail.com>
2024-03-31 00:11:25 -0400

    Update Successful

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    alert("welcome to Bimsy portfolio") 
+})
+const paragragh = document.getElementById('myparagragraph');
+const button = document.getElementById('mybutton')
+button.addEventListener('cick', function(){
+    paragragh.textContent = "this is the new content fron javasript"
+})
+
